4b81435af15e36bbf021d3e0d20bf682aba97def
Christopher <cpdregis@gmail.com>
2025-09-14 18:11:40 -0300

    bugfix2

--- src/App.tsx
+++ src/App.tsx
@@ -478,7 +478,7 @@ function App() {
 
         {/* Section 9: Download Folder */}
         <section id="baixar-folder" className="py-16 bg-gray-50">
-          <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
             <h2 className="text-3xl font-bold text-gray-800 mb-12 text-center">
               Baixe nosso folder
             </h2>
